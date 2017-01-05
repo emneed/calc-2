1258a08fcf43b86bda4950a8fc9fbda269b8b795
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 17:36:30 -0800

    Changed add to allow any num of inputs

--- arithmetic.py
+++ arithmetic.py
@@ -1,34 +1,37 @@
-def add(num1, num2):
-    return num1 + num2
+def add(list_o_nums):
+    answer = 0
+    for num in list_o_nums:
+        answer += num
+    return answer
 
 
-def subtract(num1, num2):
+def subtract(*args):
     return num1 - num2
 
 
-def multiply(num1, num2):
+def multiply(*args):
     return num1 * num2
 
 
-def divide(num1, num2):
+def divide(*args):
     # Need to turn at least argument to float for division to
     # not be integer division
     return float(num1) / float(num2)
 
 
-def square(num1):
+def square(*args):
     # Needs only one argument
-    return num1 * num1
+    return args ** 2
 
 
-def cube(num1):
+def cube(*args):
     # Needs only one argument
-    return num1 * num1 * num1
+    return args ** 3
 
 
-def power(num1, num2):
+def power(*args):
     return num1 ** num2  # ** = exponent operator
 
 
-def mod(num1, num2):
+def mod(*args):
     return num1 % num2
--- calculator.py
+++ calculator.py
@@ -12,30 +12,30 @@ from arithmetic import *
 while True:
     my_input = raw_input(">>> ")
     token = my_input.split()
-    if len(token) == 3:
-        try:
-            token[1] = int(token[1])
-            token[2] = int(token[2])
-        except:
-            print("ValueError: That is not a number!")
-    elif token[0] is 'q':
+    if token[0] is 'q':
         break
     else:
-        if token[0] is '+':
-            print(add(token[1], token[2]))
-        elif token[0] is '-':
-            print(subtract(token[1], token[2]))
-        elif token[0] is '*':
-            print(multiply(token[1], token[2]))
-        elif token[0] is '/':
-            print(divide(token[1], token[2]))
-        elif token[0] == 'square':
-            print(square(token[1], token[2]))
-        elif token[0] == 'cube':
-            print(cube(token[1], token[2]))
-        elif token[0] == 'power':
-            print(power(token[1], token[2]))
-        elif token[0] == 'mod':
-            print(mod(token[1], token[2]))
+        if len(token) > 1:
+            for item in range(1, len(token)):
+                try:
+                    token[item] = int(token[item])
+                except:
+                    print("ValueError: That is not a number!")
+            if token[0] is '+':
+                print(add(token[1:]))
+            elif token[0] is '-':
+                print(subtract(token[1], token[2]))
+            elif token[0] is '*':
+                print(multiply(token[1], token[2]))
+            elif token[0] is '/':
+                print(divide(token[1], token[2]))
+            elif token[0] == 'square':
+                print(square(token[1], token[2]))
+            elif token[0] == 'cube':
+                print(cube(token[1], token[2]))
+            elif token[0] == 'power':
+                print(power(token[1], token[2]))
+            elif token[0] == 'mod':
+                print(mod(token[1], token[2]))
         else:
             print("I don't understand.")
